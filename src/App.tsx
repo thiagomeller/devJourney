4d8b440e65a7d7f51cd95cccaf9ff9b2f243e770
LucasPaixao <lucaspaixao8@gmail.com>
2024-10-15 19:44:35 -0300

    Add rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { ThemeProvider } from "./components/ThemeProvider";
 import { Interview } from "./pages/Interview";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Login } from "./pages/Login";
 import { Roadmap } from "./pages/Roadmap";
 
 function App() {
   return (
     <ThemeProvider>
-      <div className="w-full h-full">
-        {/* <Login /> */}
-        <Interview />
-        {/*  <Roadmap /> */}
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/Interview" element={<Interview />} />
+          <Route path="/Roadmap" element={<Roadmap />} />
+          <Route path="*" element={<h1>404 - Page Not Found</h1>} />
+        </Routes>
+      </Router>
     </ThemeProvider>
   );
 }
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,16 @@
 import { Button } from "@/components/Button";
 import { Input } from "@/components/Input";
+import { useNavigate } from 'react-router-dom';
+
 
 export function Login() {
+  const navigate = useNavigate();
+  
+  const handleNavigate = () => {
+    navigate('/Interview'); 
+  };
+
+
   return (
     <>
       <div className="flex flex-col bg-secondBackground items-center justify-center rounded-sm px-28 py-32 max-w-screen-sm max-h-96 gap-10">
@@ -9,10 +18,12 @@ export function Login() {
         <Input placeholder="Usuário" />
         <Input placeholder="Senha" type="password" />
         <div className="flex flex-col gap-4 px-8">
-          <Button>Entrar</Button>
+          <Button onClick={handleNavigate}>Entrar</Button>
           <Button variant="secondary">Cadastrar-se</Button>
         </div>
       </div>
     </>
   );
 }
+
+
