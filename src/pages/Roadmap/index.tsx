28df3765dfa034753e65f3c7beaa8335e097a47c
Thiago Meller <thiagomellercasagrande@gmail.com>
2024-10-17 00:37:31 -0300

    api

--- src/pages/Roadmap/index.tsx
+++ src/pages/Roadmap/index.tsx
@@ -1,11 +1,34 @@
+import { Etapa } from "@/@types/types";
 import RoadmapComponent from "@/components/RoadmapComponent";
 import ReadmapHistory from "@/components/RoadmapHistory";
+import api from "@/service/api";
+import { useEffect, useState } from "react";
 
 export function Roadmap() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const [roadmapInfo, setRoadmapInfo] = useState<Etapa[]>([]);
+
+  const handleRoadmapInfo = async () => {
+    await api
+      .post("http://127.0.0.1:5000/v1/chat_chumbado", {})
+      .then((res) => {
+        setRoadmapInfo(res.data.Etapas);
+        console.log(res.data.Etapas);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  useEffect(() => {
+    handleRoadmapInfo();
+  }, []);
+
   return (
     <div className="flex bg-secondBackground">
       <ReadmapHistory />
-      <RoadmapComponent />
+      <RoadmapComponent roadmapInfo={roadmapInfo} />
     </div>
   );
 }
